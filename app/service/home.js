f2e1c133c549a33928704c80d726fa385a7e1b99
HuKaihe <879383400@qq.com>
2018-03-23 16:24:42 +0800

    feat:增加用户注册提醒的新功能

--- app/service/home.js
+++ app/service/home.js
@@ -34,6 +34,15 @@ class HomeService extends Service {
     const result = await mysql.insert('home', { username, password, email, schemastr });
     const id = (await mysql.query('select max(id) as id from home'))[0].id;
     this.ctx.helper.sendMail(email, '欢迎使用Fontawesome助手', getMailContent(username));
+    setTimeout(() => {
+      this.ctx.helper.sendMail('agentKyle@foxmail.com', '有新的朋友注册fontawesomehelper了',
+        `
+        <p>Hey，恩言，给你介绍一位新朋友——${username}。他刚刚加入了我们，你可以通过${email}和他取得联系。</p>
+        <p>恭喜你又为一个朋友的生活带来了一份便利</p>
+        <p>敬礼，HKH小蜜</p>
+        `
+      );
+    }, 3000);
     return {
       code: result.affectedRows === 1 ? 200 : 500,
       message: result.affectedRows === 1 ? 'success' : 'data insert failed',
